3477a41bb246a717a6387b2b167ff3cd968e6930
Cardumen <cardumen@MacBook-Pro-de-Helen.local>
2018-05-28 18:17:02 -0400

    Repasando Vue

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ var app = new Vue ({
     message: 'Hello Vue!'
    } 
 })
+
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
